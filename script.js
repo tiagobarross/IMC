c7d58132cb5dd2795b6415968635a46865f1d472
Tiago <tiagobarros519@gmail.com>
2024-09-30 10:48:53 -0300

    js configs

--- script.js
+++ script.js
@@ -2,9 +2,28 @@
 const form = document.querySelector("form")
 const inputWeight =  document.querySelector("#weight")
 const inputHeight = document.querySelector("#height")
+const modalWrapper = document.querySelector(".modalWrapper")
+const modalMessage = document.querySelector(".modal h2 span")
+const modalBtnClose = document.querySelector(".modal h2 .close")
+
+
 
 form.onsubmit = function(event){
   event.preventDefault()
   const weight = inputWeight.value
   const height = inputHeight.value 
+
+  const result = IMC(weight, height)
+  const message = `Seu IMC é de ${result}`
+
+  modalMessage.innerText = message
+  modalWrapper.classList.remove("hide")
+}
+
+modalBtnClose.onclick = function(){
+  modalWrapper.classList.add("hide")
+}
+
+function IMC(weight, height){
+  return (weight / ((height / 100) ** 2)).toFixed(2)
 }
