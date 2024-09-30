ebaa77b18ac68ab1a3ec8a535f3147aaef0cff18
Tiago <tiagobarros519@gmail.com>
2024-09-30 10:19:16 -0300

    js init

---
+++ script.js
@@ -0,0 +1,10 @@
+//variables
+const form = document.querySelector("form")
+const inputWeight =  document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+form.onsubmit = function(event){
+  event.preventDefault()
+  const weight = inputWeight.value
+  const height = inputHeight.value 
+}
